a8539628098aa46424e09a422aae9164146fa6e3
Spencer Yan <spenceryan123@gmail.com>
2017-11-28 14:04:11 -0500

    Added grid to projects.html.

---
+++ scripts/scroll.js
@@ -0,0 +1,7 @@
+// Smooth scroll to anchor location.
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
