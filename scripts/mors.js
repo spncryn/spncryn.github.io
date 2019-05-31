1cb92bd148b0719fd7cb86f82e5080fd169ea467
Spencer Yan <spenceryan123@gmail.com>
2019-05-31 02:34:57 -0400

    Trying out mobile viewports again...

--- scripts/mors.js
+++ scripts/mors.js
@@ -1,2 +1,1 @@
-var d = new Date(1997, 9, 24)
-document.getElementById("mors").innerHTML = Mors.pro(d).toFixed(2) + "%" + " | " + Mors.ndl(d) + "P" + " | " + Mors.etr(d).toFixed(0) + "R" + " | " + Mors.eyd(d)
+var d=new Date(1997,9,24);document.getElementById("mors").innerHTML=Mors.pro(d).toFixed(2)+"% | "+Mors.ndl(d)+"P | "+Mors.etr(d).toFixed(0)+"R | "+Mors.eyd(d);
