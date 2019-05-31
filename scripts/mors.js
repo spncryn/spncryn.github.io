18b8b1a414b5c64ad4e043a622d13945eee235e1
Spencer Yan <spenceryan123@gmail.com>
2019-05-31 02:28:31 -0400

    Switched Mors footer to independent JS file.

---
+++ scripts/mors.js
@@ -0,0 +1,2 @@
+var d = new Date(1997, 9, 24)
+document.getElementById("mors").innerHTML = Mors.pro(d).toFixed(2) + "%" + " | " + Mors.ndl(d) + "P" + " | " + Mors.etr(d).toFixed(0) + "R" + " | " + Mors.eyd(d)
